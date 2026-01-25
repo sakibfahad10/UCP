b2099bd52430d23148ba85ce80e2b57048c82a15
tousifahmed1 <tousifprovat15@gmail.com>
2026-01-25 12:50:13 +0600

    fix: align search icon vertically in contests page

--- components/contests-content.tsx
+++ components/contests-content.tsx
@@ -68,7 +68,7 @@ export default function ContestsContent() {
 
             {/* Search & Tabs */}
             <div className="mb-6 relative">
-              <Search className="absolute left-4 top-3.5 w-5 h-5 text-gray-400" />
+              <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
               <input
                 type="text"
                 placeholder="Search by title..."
